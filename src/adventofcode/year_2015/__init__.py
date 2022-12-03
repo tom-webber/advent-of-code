c7fdcbc702442ed36c3b15f2f31076c9bbb5df20
Tom Webber <thomas.webber@digital.justice.gov.uk>
2022-12-03 17:31:48 +0000

    remove pre-existing solutions and tests

--- src/adventofcode/year_2015/__init__.py
+++
@@ -1,3 +0,0 @@
-from adventofcode.util.helpers import get_year_from_file
-
-YEAR = get_year_from_file(__file__)
--- src/adventofcode/year_2020/__init__.py
+++
@@ -1,4 +0,0 @@
-from adventofcode.util.helpers import get_year_from_file
-
-# Bit fancy way of typing '2020', but this way I don't need to update when copied to new year
-YEAR = get_year_from_file(__file__)
